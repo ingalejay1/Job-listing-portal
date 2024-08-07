232c0509ea2b78d6d5105e9003caaaabec5dcd3b
ingalejay1 <ingalejay1@gmail.com>
2024-08-07 11:59:36 +0530

    second commit

--- index.js
+++ index.js
@@ -3,17 +3,33 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 const authRoutes = require("./routes/auth");
 dotenv.config();
 const PORT = process.env.PORT;
+
+//log every incoming request
+// app.use((req, res, next) => {
+//     console.log(`${req.method} - ${req.url} - ${new Date()}`);
+// });
+
+
 
 
 app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
+
+//error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    console.log(`${req.method} - ${req.url} - ${new Date()}`);
+    res.status(500).send("Something broke!");
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on the port ${PORT}`);
--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,18 +1,49 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../../schemas/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+router.post("/login", async(req, res,next ) => {
+    try {
+        // throw new Error("This is an error");
+        const {email, password} = req.body;
+        const user = await User.findOne({email});
+        if(!user) {
+            res.status(400).json({message: "Wrong email or password"});
+        }
+        const validPassword = await bcrypt.compare(password, user.password);
+        if (!validPassword) {
+            res.status(400).json({ message: "Wrong email or password"})
+        }
+        else {
+            const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+            res.header("auth-token", token).json({ message: "Logged in Successfully" });
+        }
+    }
+    catch (err) {
+        next(err);
+    }
+    
+})
 
 
 router.post("/register", async (req, res) => {
     const {name, email, password, mobile} = req.body;
+    console.log(req.body);
     const user = await User.findOne({email});
     if(user){
         return res.status(400).json({message: "User already exists"});
     }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
     const newUser = new User({
         name, 
         email,
-        password,
+        password: hashedPassword,
         mobile
     });
     await newUser.save();
